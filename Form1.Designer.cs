5e49ca703c2bb7275a6115fca8f13b6513bc9099
dotada <luboslav.blazhev@gmail.com>
2023-08-27 00:53:29 +0300

    a lot of changes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,13 +36,16 @@ namespace WindowsFormsApp1
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(12, 164);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(467, 25);
+            this.button1.Size = new System.Drawing.Size(242, 25);
             this.button1.TabIndex = 0;
             this.button1.Text = "DOWNLOAD";
             this.button1.UseVisualStyleBackColor = true;
@@ -75,14 +78,14 @@ namespace WindowsFormsApp1
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(12, 86);
+            this.textBox3.Location = new System.Drawing.Point(12, 98);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(396, 20);
             this.textBox3.TabIndex = 3;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(414, 86);
+            this.button2.Location = new System.Drawing.Point(414, 95);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 4;
@@ -90,11 +93,44 @@ namespace WindowsFormsApp1
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(260, 164);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(219, 25);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "DOWNLOAD (HIGHEST QUALITY)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 69);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(467, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "README IMPORTANT!";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 39);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(467, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Download ffmpeg";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(491, 197);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
@@ -117,6 +153,9 @@ namespace WindowsFormsApp1
         private FolderBrowserDialog folderBrowserDialog1;
         private TextBox textBox3;
         private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,12 +12,16 @@ using YoutubeExplode;
 using YoutubeExplode.Videos.Streams;
 using System.Text.RegularExpressions;
 using AngleSharp.Common;
+using YoutubeExplode.Converter;
+using YoutubeExplode.Videos;
+using System.Net;
+using System.IO.Compression;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        public Form1()  
         {
             InitializeComponent();
         }
@@ -61,8 +65,9 @@ namespace WindowsFormsApp1
                 var streamInfo = streamManifest.GetMuxedStreams().GetWithHighestVideoQuality();
                 _ = await youtube.Videos.Streams.GetAsync(streamInfo);
                 string sanitizedtitle = SanitizeFileName(video.Title);
-                Console.WriteLine($"{textBox3.Text}\\{sanitizedtitle}.{streamInfo.Container}");
                 await youtube.Videos.Streams.DownloadAsync(streamInfo, $"{textBox3.Text}\\{sanitizedtitle}.{streamInfo.Container}");
+                Form7 done = new Form7();
+                done.ShowDialog();
             } else {
                 Form2 invalidurl = new Form2();
                 invalidurl.ShowDialog();
@@ -79,6 +84,67 @@ namespace WindowsFormsApp1
                     textBox3.Text = fbd.SelectedPath;
                 }
             }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form3 readme = new Form3();
+            readme.ShowDialog();
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            if (Uri.IsWellFormedUriString(textBox2.Text, UriKind.Absolute))
+            {
+                string folder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                string specificFolder = Path.Combine(folder, "DYVD");
+                string ffmpegloc = Path.Combine(specificFolder, "ffmpeg-master-latest-win64-gpl-shared");
+                string ffmpegclidir = Path.Combine(ffmpegloc, "bin");
+                string ffmpegcli = Path.Combine(ffmpegclidir, "ffmpeg.exe");
+                if (Directory.Exists(ffmpegclidir))
+                {
+                    var youtube = new YoutubeClient();
+                    var videourl = textBox2.Text;
+                    var video = await youtube.Videos.GetAsync(videourl);
+                    string sanitizedtitle = SanitizeFileName(video.Title);
+                    var filepath = $"{textBox3.Text}\\{sanitizedtitle}.mp4";
+                    await youtube.Videos.DownloadAsync(videourl, filepath, o => o
+                        .SetContainer("mp4")
+                        .SetFFmpegPath(ffmpegcli)
+                    );
+                    Form7 done = new Form7();
+                    done.ShowDialog();
+                } else
+                {
+                    Form5 form5 = new Form5();
+                    form5.ShowDialog();
+                }
+            } else {
+                Form2 invalidurl = new Form2();
+                invalidurl.ShowDialog();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string folder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string specificFolder = Path.Combine(folder, "DYVD");
+            string ffmpegloc = Path.Combine(specificFolder, "ffmpeg-master-latest-win64-gpl-shared");
+            string ffmpegdw = Path.Combine(specificFolder, "ffmpeg-master-latest-win64-gpl-shared.zip");
+            if (Directory.Exists(ffmpegloc))
+            {
+                Form4 ffmpeginstalled = new Form4();
+                ffmpeginstalled.ShowDialog();
+            } else
+            {
+                Directory.CreateDirectory(specificFolder);
+                WebClient webc = new WebClient();
+                webc.DownloadFile("https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl-shared.zip", ffmpegdw);
+                ZipFile.ExtractToDirectory(ffmpegdw, specificFolder);
+                Form6 form6 = new Form6();
+                form6.ShowDialog();
+            }
+            
         }
     }
 }
